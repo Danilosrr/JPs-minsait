8ce5ce1edc0b89f446152b5bee0b4ae70dd272c2
Danilosrr <danilo_srr@hotmail.com>
2023-10-27 00:04:44 -0300

    initial commit

---
+++ JPs_backend/src/main/java/com/minsait/JPs_backend/JPsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.minsait.JPs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JPsBackendApplication.class, args);
+	}
+
+}
