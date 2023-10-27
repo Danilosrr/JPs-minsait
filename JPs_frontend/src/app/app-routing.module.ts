6b9b1b6a7ae3b671af366fddb2dd4a110244d0d0
Danilosrr <danilo_srr@hotmail.com>
2023-10-27 18:23:18 -0300

    components inited, routing made

--- JPs_frontend/src/app/app-routing.module.ts
+++ JPs_frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
+import { CadastrarProdutosComponent } from './pages/cadastrar-produtos/cadastrar-produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'produtos', component: ProdutosComponent },
+  { path: 'produtos/cadastrar', component: CadastrarProdutosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- JPs_frontend/src/app/app.module.ts
+++ JPs_frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CadastrarProdutosComponent } from './pages/cadastrar-produtos/cadastrar-produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProdutosComponent,
+    HeaderComponent,
+    CadastrarProdutosComponent
   ],
   imports: [
     BrowserModule,
