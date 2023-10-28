f057f09a60026be82828ca41e2ed7f298495d95a
Danilosrr <danilo_srr@hotmail.com>
2023-10-28 00:45:18 -0300

    list of products paginated

--- JPs_frontend/src/app/pages/produtos/produtos.component.ts
+++ JPs_frontend/src/app/pages/produtos/produtos.component.ts
@@ -5,20 +5,51 @@ import { ProdutosService } from 'src/app/services/produtos.service';
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
-  styleUrls: ['./produtos.component.css']
+  styleUrls: ['./produtos.component.css'],
 })
 export class ProdutosComponent {
-  produtos: IProduto[] = []
+  produtos: IProduto[] = [];
+  produtosPaginado: IProduto[] = [];
+  pagina: number = 1;
+  ultimaPagina: number = 1;
+  tamanhoPagina: number = 3;
+
   constructor(private produtosService: ProdutosService) {}
 
   ngOnInit() {
     this.produtosService.buscarTodos().subscribe(
       (produtos) => {
         this.produtos = produtos;
+        this.paginar(this.pagina);
+        this.ultimaPagina = Math.ceil(produtos.length / this.tamanhoPagina);
       },
       (error) => {
         console.log(error);
       }
     );
+  }
+
+  paginar(pagina: number) {
+    const indexFinal = pagina * this.tamanhoPagina;
+    const indexInicial = indexFinal - this.tamanhoPagina;
+    this.produtosPaginado = this.produtos.slice(indexInicial, indexFinal);
+  }
+
+  paginaProx() {
+    if (this.pagina == this.ultimaPagina) return;
+    this.pagina++;
+    this.paginar(this.pagina);
+  }
+
+  paginaAnt() {
+    if (this.pagina == 1) return;
+    this.pagina--;
+    this.paginar(this.pagina);
+  }
+
+  paginaX(pagina: number) {
+    if (this.pagina == pagina) return;
+    this.pagina = pagina;
+    this.paginar(this.pagina);
   }
 }
